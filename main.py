8018313397f93dd3f4a4604a7e05191aec7f560f
nikit <nikitin.bogdan333@yandex.ru>
2019-11-12 16:11:25 +0300

    Первый вариант решения задачи "Git и случайные окружности"

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import sys
 import random
+from UI import Ui_MainWindow
 from PyQt5 import uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtGui import QPainter, QColor, QPen
 from PyQt5.QtCore import Qt
 
 
-class CirclesMainWindow(QMainWindow):
+class CirclesMainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.circles = []
         self.draw_btn.clicked.connect(self.add_circle)
 
@@ -17,14 +18,15 @@ class CirclesMainWindow(QMainWindow):
         diameter = random.randint(1, min([self.width(), self.height()]))
         x = random.randint(0, self.width() - diameter - 1)
         y = random.randint(0, self.height() - diameter - 1)
-        self.circles += [(x, y, diameter, diameter)]
+        color = [random.randint(0, 255) for _ in range(3)]
+        self.circles += [(color, (x, y, diameter, diameter))]
         self.update()
 
     def draw_circles(self, qp):
         qp.setBrush(QColor(0, 0, 0, 0))
-        pen = QPen(Qt.yellow, 5)
-        qp.setPen(pen)
-        for data in self.circles:
+        for color, data in self.circles:
+            pen = QPen(QColor(*color), 5)
+            qp.setPen(pen)
             qp.drawEllipse(*data)
 
     def paintEvent(self, *args, **kwargs):
